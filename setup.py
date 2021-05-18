9218e5c9f59ce8aa192d6539a8ec9a5f8b23f468
Alex Orid <TheCuriousNerd@users.noreply.github.com>
2021-05-17 22:44:57 -0400

    Description Fix

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ with open("README.md", "r", encoding="utf-8") as file:
 setup(
     name='BadWordObfuscator',
     packages=find_packages(include=['BadWordObfuscator']),
-    version='0.1.2',
+    version='0.1.3',
     description='A package that helps make naughty words less visible in code.',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Alex Orid',
     author_email='inquiries@thecuriousnerd.com',
     url='https://github.com/TheCuriousNerd/Bad-Word-Obfuscator',
