b3468c15cc0052b433f8820638a209e02b8eec3b
Alex Orid <TheCuriousNerd@users.noreply.github.com>
2021-05-17 22:26:56 -0400

    Updated Description

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from os import name
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
 setup(
     name='BadWordObfuscator',
     packages=find_packages(include=['BadWordObfuscator']),
-    version='0.1.1',
-    description='This code obfuscates naughty words by using a rot13 cipher to prevent them from being directly visible.',
+    version='0.1.2',
+    description='A package that helps make naughty words less visible in code.',
+    long_description=long_description,
     author='Alex Orid',
     author_email='inquiries@thecuriousnerd.com',
     url='https://github.com/TheCuriousNerd/Bad-Word-Obfuscator',
